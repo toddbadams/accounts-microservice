ec6cac36f940c0bc92bca28471daa293a564686f
Todd B. Adams <toddbadams@msn.com>
2015-05-15 00:26:43 +0100

    added exception handling

---
+++ tba.Core/Exceptions/EntityDoesNotExistException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace tba.Core.Exceptions
+{
+    public class EntityDoesNotExistException : Exception
+    {
+        public EntityDoesNotExistException()
+        {
+
+        }
+
+        public EntityDoesNotExistException(string message)
+            : base(message)
+        {
+
+        }
+
+        public EntityDoesNotExistException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+    }
+}
